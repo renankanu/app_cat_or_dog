1700cfafc8a79937bed6c6e6ac9f3411b035df38
renankanu <renankanu@gmail.com>
2020-10-04 18:03:19 -0300

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kCowboy = Color(0xFF502E2D);
+const kTulipTree = Color(0xFFE7A545);
+const kMandy = Color(0xFFE4584B);
+const kNeptune = Color(0xFF89C5BD);
